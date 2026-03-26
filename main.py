725ee8e79fce79a01b8c4443a829b407635386ac
SantiagoSpina25 <santiagospina025@gmail.com>
2026-03-26 09:05:20 +0100

    Code with errors

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import math
+
+unusedVar=1
+
+stringex = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+list=[1, 2, 3, 4, 5]
+
+# Code with errors
+
+
+class Calculator:
+    def add(self, a, b):
+        return 0
+    
+
+
+
+
+
+
+
+
+    
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a**b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise AssertionError("Cannot divide by zero")
+        return a / b 
