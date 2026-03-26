ebc44911d0ed124372185b2d27f2ead6fee2fcea
SantiagoSpina25 <santiagospina025@gmail.com>
2026-03-26 09:50:12 +0100

    Linting and tests fix

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-import os
-import math
-
-unusedVar = 1
-
-list = [1, 2, 3, 4, 5]
-
-# Code with errors
-
-print("testing")
+# Code without errors
 
 
 class Calculator:
     def add(self, a, b):
-        return 0
+        return a + b
 
     def subtract(self, a, b):
         return a - b
 
     def multiply(self, a, b):
-        return a**b
+        return a * b
 
     def divide(self, a, b):
         if b == 0:
-            raise AssertionError("Cannot divide by zero")
+            raise ValueError("Cannot divide by zero")
         return a / b
